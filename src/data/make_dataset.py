fd39e59eb4b09e9d327434d5d38856a31f1303e2
Rasmus Spangsberg <rasmus.spangsberg@hotmail.com>
2022-01-05 19:28:09 +0100

    updated make_dataset.py to output files of a tensor at data/processed/corruptmnist and updated requirements.txt

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,26 +4,33 @@ import os
 
 
 def mnist():
-    root = r"/Users/rasmusspangsberg/Documents/DTU/semester1/3uger/dtu_mlops/data/corruptmnist/"
+    raw_data_path = r"data\raw\corruptmnist"
+    processed_data_path = r"data\processed\corruptmnist"
+
+    if not os.path.exists(processed_data_path):
+        os.mkdir(processed_data_path)
 
     # train data set, we have 5 different data sets
     images = np.empty([0, 1, 28, 28], dtype=np.float32)
-    labels = np.empty([0], dtype=np.long)
+    labels = np.empty([0], dtype=np.compat.long)
     for i in range(5):
-        path = os.path.join(root, "train_" + str(i) + ".npz")
+        path = os.path.join(raw_data_path, "train_" + str(i) + ".npz")
         with np.load(path) as data:
             a = data["images"]
             a = a.reshape(a.shape[0], 1, 28, 28)
             a = a.astype(np.float32)
             images = np.concatenate((images, a), axis=0)
-            labels = np.concatenate((labels, data["labels"].astype(np.long)))
+            labels = np.concatenate((labels, data["labels"].astype(np.compat.long)))
 
-    train_data_set = torch.utils.data.TensorDataset(
-        torch.from_numpy(images), torch.from_numpy(labels))
-    train_loader = torch.utils.data.DataLoader(train_data_set, batch_size=64, shuffle=True)
+    # convert to torch tensors
+    images = torch.from_numpy(images)
+    labels = torch.from_numpy(labels)
+
+    torch.save(images, os.path.join(processed_data_path, "train_image.pth"))
+    torch.save(labels, os.path.join(processed_data_path, "train_labels.pth"))
 
     # test data set, only a single data set
-    path = os.path.join(root, "test.npz")
+    path = os.path.join(raw_data_path, "test.npz")
 
     with np.load(path) as data:
         images = data["images"]
@@ -31,8 +38,13 @@ def mnist():
         images = images.astype(np.float32)
         labels = data["labels"]
 
-    test_data_set = torch.utils.data.TensorDataset(
-        torch.from_numpy(images), torch.from_numpy(labels))
-    test_loader = torch.utils.data.DataLoader(test_data_set, batch_size=len(test_data_set), shuffle=False)
+    # convert to torch tensors
+    images = torch.from_numpy(images)
+    labels = torch.from_numpy(labels)
 
-    return train_loader, test_loader
+    torch.save(images, os.path.join(processed_data_path, "test_image.pth"))
+    torch.save(labels, os.path.join(processed_data_path, "test_labels.pth"))
+
+
+if __name__ == "__main__":
+    mnist()
